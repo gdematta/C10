2ef7d61cd6cc87353b65eca2a960c0d67736c744
sgroth88 <sgroth@mines.edu>
2025-02-12 17:22:28 -0700

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,22 @@
-public class Student {
-	
+public class Student implements Comparable<Student> {
+    private String name;
+    private int score;
+
+    // Constructor
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    // Compare students based on score
+    @Override
+    public int compareTo(Student other) {
+        return Integer.compare(this.score, other.score);
+    }
+
+    // String representation of a student
+    @Override
+    public String toString() {
+        return name + ", " + score;
+    }
 }
